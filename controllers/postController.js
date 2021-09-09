bd753979ae77151b2adec7946aad847274026953
javimarashall <jamondragon@ucdavis.edu>
2021-09-09 14:53:10 -0700

    single post route

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,14 +1,41 @@
 const db = require("../models");
+const {User} = require("../models");
 require('dotenv').config();
 
 module.exports = {
-//=======================================ROUTES WORKS=========================================FINDS ALL POSTS============    
+    //=======================================ROUTES WORKS=========================================FINDS ALL POSTS============    
     findAll: function (req, res) {
         db.Post.findAll({})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
-//=======================================ROUTES WORKS=========================================FINDS ONE USER ON LOGIN====
+
+
+    getPost: async function (req, res) {
+      try {
+          const postData = await db.Post.findOne({ where: {id: req.params.id}});
+          const commentData = await db.Comment.findAll(
+              {
+                  where: {
+                      post_id: req.params.id
+                  },
+                  include: [
+                      { model: User}
+                  ],
+              }
+          );
+          const comments = commentData.map((comment) => comment.get({plain: true}));
+          console.log(comments);
+          if (!postData) {
+              res.status(404).json({message: "No Post Found Matching Your Selection"});
+          }
+          res.status(200).json({post: postData, comment: commentData});
+      }catch (err) {
+          console.log(err);
+          res.status(500).json(err);
+      }
+    },
+    //=======================================ROUTES WORKS=========================================FINDS ONE USER ON LOGIN====
     findOne: async function (req, res) {
 
         try {
@@ -33,11 +60,31 @@ module.exports = {
             res.status(400).json(err);
         }
     },
+    //=======================================ROUTES WORKS=========================================CREATES A NEW USER
     create: function (req, res) {
         db.User.create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    //=======================================ROUTES WORKS=========================================CREATES A NEW COMMENT
+    postComment: async function (req, res) {
+        const goodParams = { ...req.body, user_id: req.session.user_id }
+        try {
+            const commentData = await db.Comment.create(goodParams);
+            console.log('*******review Created');
+            res.status(200).json(commentData);
+        } catch (err) {
+            res.status(400).json(err);
+        }
     }
 
-
 }
+
+//=======================================ROUTES WORKS=========================================FINDS ALL USERS
+    // getUsers: function (req, res) {
+    //     db.User.findAll({})
+    //       // .sort({ _id: -1 })
+    //       .then(dbModel => res.json(dbModel))
+    //       .catch(err => res.status(422).json(err));
+    //   },
--- routes/API.js
+++ routes/API.js
@@ -6,13 +6,26 @@ router
     .route("/")
     .get(postController.findAll);
 
+router
+    .route("/:id")
+    .get(postController.getPost);
+
 //=======================================ROUTES WORKS=========================================    
 router
     .route("/login")
     .post(postController.findOne);
-
+//=======================================ROUTES WORKS=========================================
 router
     .route("/register")
     .post(postController.create);
 
+router
+    .route("/comment")
+    .post(postController.postComment)
+
 module.exports = router;
+
+//=======================================ROUTES WORKS=========================================
+// router
+//     .route("/users")
+//     .get(postController.getUsers);
