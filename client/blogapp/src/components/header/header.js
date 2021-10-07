f144847e359a5100de140cf62d1580dba92212c3
javimarashall <jamondragon@ucdavis.edu>
2021-10-06 19:16:09 -0700

    app and index done

---
+++ client/blogapp/src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header(props) {
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid text-center">
+                <div className="container">
+                    <p className="lead">Search twitter and let us analyze its meaning!!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
